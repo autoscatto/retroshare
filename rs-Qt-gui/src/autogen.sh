921e7fb9c9c11ee5cb04ac452537906cf2b875ab
defnax <defnax@b45a01b8-16f6-495d-af2f-9b41ad6348cc>
2007-05-06 22:51:05 +0000

    remove not needed sources
    git-svn-id: svn://svn.code.sf.net/p/retroshare/code@14 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- rs-Qt-gui/src/autogen.sh
+++
@@ -1,9 +0,0 @@
-echo "Creating project file"
-qmake -project -o retroshare.pro
-echo "Running script to fix project file"
-./script/script.pl retroshare.pro
-echo "Making Makefiles"
-qmake retroshare.pro
-echo "You can now build retroshare by running make"
-
-
