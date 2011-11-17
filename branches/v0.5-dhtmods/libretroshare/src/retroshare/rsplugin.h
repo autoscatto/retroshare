1e6e5df42ec6f810bd2af793e675603f8a8a9cdc
drbob7 <drbob7@b45a01b8-16f6-495d-af2f-9b41ad6348cc>
2011-11-17 22:03:44 +0000

    Branch to for DHT improvements. Planned improvements in this branch: 	- Fix Manual Port. 	- Exchange/Block Bad IP Addresses. 	- Handle Multiple DHT Entries. 	- Add Friends Lists. 	- Intermittent Search 	- Enable Relay System.
    git-svn-id: svn://svn.code.sf.net/p/retroshare/code@4674 b45a01b8-16f6-495d-af2f-9b41ad6348cc

---
+++ branches/v0.5-dhtmods/libretroshare/src/retroshare/rsplugin.h
@@ -0,0 +1,117 @@
+/*
+ * "$Id: rsiface.h,v 1.9 2007-04-21 19:08:51 rmf24 Exp $"
+ *
+ * RetroShare C++ Interface.
+ *
+ * Copyright 2011-2011 by Cyril Soler
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License Version 2 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ *
+ * Please report all bugs and problems to "retroshare@lunamutt.com".
+ *
+ */
+
+#pragma once
+
+#include <time.h>
+#include <stdint.h>
+#include <string>
+#include <vector>
+#include "retroshare/rspeers.h"
+#include "retroshare/rsfiles.h"
+
+class RsPluginHandler ;
+extern RsPluginHandler *rsPlugins ;
+
+class p3Service ;
+class p3LinkMgr ;
+class MainPage ;
+class QIcon ;
+class QString ;
+class QWidget ;
+class QTranslator;
+class QApplication;
+class RsCacheService ;
+class ftServer ;
+class pqiService ;
+
+// Used for the status of plugins.
+//
+#define PLUGIN_STATUS_NO_STATUS      0x0000
+#define PLUGIN_STATUS_UNKNOWN_HASH   0x0001
+#define PLUGIN_STATUS_DLOPEN_ERROR   0x0002
+#define PLUGIN_STATUS_MISSING_SYMBOL 0x0003
+#define PLUGIN_STATUS_NULL_PLUGIN    0x0004
+#define PLUGIN_STATUS_LOADED         0x0005
+
+class RsPluginHandler;
+
+/*!
+ *
+ * convenience class to store gui interfaces
+ *
+ */
+class RsPlugInInterfaces {
+
+public:
+
+    RsPlugInInterfaces() { mPeers = NULL; mFiles = NULL; }
+    RsPeers* mPeers;
+    RsFiles* mFiles;
+
+};
+
+class RsPlugin
+{
+	public:
+		virtual RsCacheService *rs_cache_service() 	const	{ return NULL ; }
+		virtual pqiService     *rs_pqi_service() 		const	{ return NULL ; }
+		virtual uint16_t        rs_service_id() 	   const	{ return 0    ; }
+
+		virtual MainPage       *qt_page()       		const	{ return NULL ; }
+		virtual QWidget        *qt_config_panel()		const	{ return NULL ; }
+		virtual QIcon          *qt_icon()       		const	{ return NULL ; }
+		virtual QTranslator    *qt_translator(QApplication *app, const QString& languageCode) const	{ return NULL ; }
+
+		virtual std::string configurationFileName() const { return std::string() ; }
+		virtual std::string getShortPluginDescription() const = 0 ;
+		virtual std::string getPluginName() const = 0 ;
+		virtual void getPluginVersion(int& major,int& minor,int& svn_rev) const = 0 ;
+		virtual void setPlugInHandler(RsPluginHandler* pgHandler) = 0;
+		virtual void setInterfaces(RsPlugInInterfaces& interfaces) = 0;
+};
+
+class RsPluginHandler
+{
+	public:
+		// Returns the number of loaded plugins.
+		//
+		virtual int nbPlugins() const = 0 ;
+		virtual RsPlugin *plugin(int i) = 0 ;
+		virtual const std::vector<std::string>& getPluginDirectories() const = 0;
+		virtual void getPluginStatus(int i,uint32_t& status,std::string& file_name,std::string& file_hash,std::string& error_string) const = 0 ;
+		virtual void enablePlugin(const std::string& hash) = 0;
+		virtual void disablePlugin(const std::string& hash) = 0;
+
+		virtual void slowTickPlugins(time_t sec) = 0 ;
+
+		virtual const std::string& getLocalCacheDir() const =0;
+		virtual const std::string& getRemoteCacheDir() const =0;
+		virtual ftServer *getFileServer() const = 0;
+		virtual p3LinkMgr *getLinkMgr() const = 0;
+};
+
+
+
---
+++ branches/v0.5-dhtmods/retroshare-gui/src/gui/feeds/PeerItem.cpp
@@ -0,0 +1,349 @@
+/****************************************************************
+ *  RetroShare is distributed under the following license:
+ *
+ *  Copyright (C) 2008 Robert Fernie
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ *  Boston, MA  02110-1301, USA.
+ ****************************************************************/
+
+#include <QDateTime>
+#include <QTimer>
+
+#include "PeerItem.h"
+#include "FeedHolder.h"
+#include "../RsAutoUpdatePage.h"
+#include "gui/msgs/MessageComposer.h"
+#include "gui/common/StatusDefs.h"
+#include "gui/common/AvatarDefs.h"
+
+#include "gui/notifyqt.h"
+
+#include <retroshare/rsmsgs.h>
+#include <retroshare/rspeers.h>
+
+#include <sstream>
+
+/*****
+ * #define DEBUG_ITEM 1
+ ****/
+
+/** Constructor */
+PeerItem::PeerItem(FeedHolder *parent, uint32_t feedId, std::string peerId, uint32_t type, bool isHome)
+:QWidget(NULL), mParent(parent), mFeedId(feedId),
+	mPeerId(peerId), mType(type), mIsHome(isHome)
+{
+    /* Invoke the Qt Designer generated object setup routine */
+    setupUi(this);
+  
+    messageframe->setVisible(false);
+    sendmsgButton->setEnabled(false);
+
+    /* general ones */
+    connect( expandButton, SIGNAL( clicked( void ) ), this, SLOT( toggle ( void ) ) );
+    connect( clearButton, SIGNAL( clicked( void ) ), this, SLOT( removeItem ( void ) ) );
+
+    /* specific ones */
+    connect( chatButton, SIGNAL( clicked( void ) ), this, SLOT( openChat ( void ) ) );
+    connect( actionNew_Message, SIGNAL( triggered( ) ), this, SLOT( sendMsg ( void ) ) );
+
+    connect( quickmsgButton, SIGNAL( clicked( ) ), this, SLOT( togglequickmessage() ) );
+    connect( cancelButton, SIGNAL( clicked( ) ), this, SLOT( togglequickmessage() ) );
+
+    connect( sendmsgButton, SIGNAL( clicked( ) ), this, SLOT( sendMessage() ) );
+
+    connect(NotifyQt::getInstance(), SIGNAL(friendsChanged()), this, SLOT(updateItem()));
+
+    QMenu *msgmenu = new QMenu();
+    msgmenu->addAction(actionNew_Message);
+
+    quickmsgButton->setMenu(msgmenu);
+
+    avatar->setId(mPeerId, false);
+
+    small();
+    updateItemStatic();
+    updateItem();
+}
+
+
+void PeerItem::updateItemStatic()
+{
+	if (!rsPeers)
+		return;
+
+	/* fill in */
+#ifdef DEBUG_ITEM
+	std::cerr << "PeerItem::updateItemStatic()";
+	std::cerr << std::endl;
+#endif
+
+	QString title;
+
+	switch(mType)
+	{
+		case PEER_TYPE_STD:
+							title = tr("Friend");
+			break;
+		case PEER_TYPE_CONNECT:
+							title = tr("Friend Connected");
+			break;
+		case PEER_TYPE_HELLO:
+							title = tr("Connect Attempt");
+			break;
+		case PEER_TYPE_NEW_FOF:
+							title = tr("Friend of Friend");
+			break;
+		default:
+							title = tr("Peer");
+			break;
+	}
+
+	titleLabel->setText(title);
+
+	/* set textcolor for peer name  */
+	QString nameStr("<span style=\"font-size:14pt; font-weight:500;color:#990033;\">%1</span>");
+
+	RsPeerDetails details;
+	if (rsPeers->getPeerDetails(mPeerId, details))
+	{
+		/* set peer name */
+		QString peername =  QString::fromUtf8(details.name.c_str());
+		peernameLabel->setText(nameStr.arg(peername));
+
+		QDateTime date = QDateTime::fromTime_t(details.lastConnect);
+		QString stime = date.toString(Qt::LocalDate);
+		lastLabel-> setText(stime);
+
+		/* expanded Info */
+		nameLabel->setText(QString::fromUtf8(details.name.c_str()));
+		idLabel->setText(QString::fromStdString(details.id));
+		locLabel->setText(QString::fromUtf8(details.location.c_str()));
+	}
+	else
+	{
+		statusLabel->setText(tr("Unknown Peer"));
+		titleLabel->setText(tr("Unknown Peer"));
+		trustLabel->setText(tr("Unknown Peer"));
+		nameLabel->setText(tr("Unknown Peer"));
+		idLabel->setText(tr("Unknown Peer"));
+		locLabel->setText(tr("Unknown Peer"));
+		ipLabel->setText(tr("Unknown Peer"));
+		connLabel->setText(tr("Unknown Peer"));
+		lastLabel->setText(tr("Unknown Peer"));
+
+		chatButton->setEnabled(false);
+	}
+
+	if (mIsHome)
+	{
+		/* disable buttons */
+		clearButton->setEnabled(false);
+
+		/* hide buttons */
+		clearButton->hide();
+	}
+}
+
+void PeerItem::updateItem()
+{
+	if (!rsPeers)
+		return;
+
+	/* fill in */
+#ifdef DEBUG_ITEM
+	std::cerr << "PeerItem::updateItem()";
+	std::cerr << std::endl;
+#endif
+	if(!RsAutoUpdatePage::eventsLocked()) {
+		/* set textcolor for peer name  */
+		QString nameStr("<span style=\"font-size:14pt; font-weight:500;color:#990033;\">%1</span>");
+
+		RsPeerDetails details;
+		if (!rsPeers->getPeerDetails(mPeerId, details))
+		{
+			chatButton->setEnabled(false);
+			quickmsgButton->setEnabled(false);
+
+			return;
+		}
+
+		/* top Level info */
+		QString status = StatusDefs::peerStateString(details.state);
+
+#if 0
+		/* Append additional status info from status service */
+		StatusInfo statusInfo;
+		if ((rsStatus) && (rsStatus->getStatus(*it, statusInfo)))
+		{
+			status.append(QString::fromStdString("/" + RsStatusString(statusInfo.status)));
+		}
+#endif
+		statusLabel->setText(status);
+		trustLabel->setText(QString::fromStdString(RsPeerTrustString(details.trustLvl)));
+
+		{
+			std::ostringstream out;
+			out << details.localAddr << ":";
+			out << details.localPort << "/";
+			out << details.extAddr << ":";
+			out << details.extPort;
+			ipLabel->setText(QString::fromStdString(out.str()));
+		}
+
+		connLabel->setText(StatusDefs::connectStateString(details));
+
+		/* do buttons */
+		chatButton->setEnabled(details.state & RS_PEER_STATE_CONNECTED);
+		if (details.state & RS_PEER_STATE_FRIEND)
+		{
+			quickmsgButton->setEnabled(true);
+		}
+		else
+		{
+			quickmsgButton->setEnabled(false);
+		}
+	}
+
+	/* slow Tick  */
+	int msec_rate = 10129;
+	
+	QTimer::singleShot( msec_rate, this, SLOT(updateItem( void ) ));
+	return;
+}
+
+void PeerItem::small()
+{
+	expandFrame->hide();
+}
+
+void PeerItem::toggle()
+{
+	if (expandFrame->isHidden())
+	{
+		expandFrame->show();
+		expandButton->setIcon(QIcon(QString(":/images/edit_remove24.png")));
+		expandButton->setToolTip(tr("Hide"));
+	}
+	else
+	{
+		expandFrame->hide();
+		expandButton->setIcon(QIcon(QString(":/images/edit_add24.png")));
+		expandButton->setToolTip(tr("Expand"));
+	}
+}
+
+
+void PeerItem::removeItem()
+{
+#ifdef DEBUG_ITEM
+	std::cerr << "PeerItem::removeItem()";
+	std::cerr << std::endl;
+#endif
+	hide();
+	if (mParent)
+	{
+		mParent->deleteFeedItem(this, mFeedId);
+	}
+}
+
+/*********** SPECIFIC FUNCTIOSN ***********************/
+
+void PeerItem::addFriend()
+{
+#ifdef DEBUG_ITEM
+	std::cerr << "PeerItem::addFriend()";
+	std::cerr << std::endl;
+#endif
+}
+
+void PeerItem::removeFriend()
+{
+#ifdef DEBUG_ITEM
+	std::cerr << "PeerItem::removeFriend()";
+	std::cerr << std::endl;
+#endif
+}
+
+void PeerItem::sendMsg()
+{
+#ifdef DEBUG_ITEM
+	std::cerr << "PeerItem::sendMsg()";
+	std::cerr << std::endl;
+#endif
+
+    MessageComposer *nMsgDialog = MessageComposer::newMsg();
+    if (nMsgDialog == NULL) {
+        return;
+    }
+
+    nMsgDialog->addRecipient(MessageComposer::TO, mPeerId, false);
+    nMsgDialog->show();
+    nMsgDialog->activateWindow();
+
+    /* window will destroy itself! */
+}
+
+
+void PeerItem::openChat()
+{
+#ifdef DEBUG_ITEM
+	std::cerr << "PeerItem::openChat()";
+	std::cerr << std::endl;
+#endif
+	if (mParent)
+	{
+		mParent->openChat(mPeerId);
+	}
+}
+
+void PeerItem::togglequickmessage()
+{
+	if (messageframe->isHidden())
+	{
+        messageframe->setVisible(true);
+    }
+	else
+	{
+        messageframe->setVisible(false);
+    }	
+}
+
+void PeerItem::sendMessage()
+{
+    /* construct a message */
+    MessageInfo mi;
+    
+    mi.title = tr("Quick Message").toStdWString();
+    mi.msg =   quickmsgText->toHtml().toStdWString();
+    mi.msgto.push_back(mPeerId);       
+    
+    rsMsgs->MessageSend(mi);
+
+    quickmsgText->clear();
+    messageframe->setVisible(false);
+}
+
+void PeerItem::on_quickmsgText_textChanged()
+{
+    if (quickmsgText->toPlainText().isEmpty())
+    {
+        sendmsgButton->setEnabled(false);
+    }
+    else
+    {
+        sendmsgButton->setEnabled(true);
+    }
+}
